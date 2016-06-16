c1f6144e0e26324067d458084be658db84d06682
Tran Situ <tsitu13@gmail.com>
2016-06-15 18:57:25 -0700

    Initial commit for Marketplace Analyzer

---
+++ src/analyzer.js
@@ -0,0 +1,188 @@
+"use strict";
+
+window.onload = function () {
+
+	//Initialize tablesorter, bind to table
+	$.tablesorter.defaults.sortInitialOrder = 'desc';
+    $("#table").tablesorter({
+		sortReset: true,
+		widthFixed: true,
+		ignoreCase: false,
+		widgets: ["filter", "pager"],
+		widgetOptions: {
+			filter_childRows : false,
+			filter_childByColumn : false,
+			filter_childWithSibs : true,
+			filter_columnFilters : true,
+			filter_columnAnyMatch: true,
+			filter_cellFilter : '',
+			filter_cssFilter : '', // or []
+			filter_defaultFilter : {},
+			filter_excludeFilter : {},
+			filter_external : '',
+			filter_filteredRow : 'filtered',
+			filter_formatter : null,
+			filter_functions : null,
+			filter_hideEmpty : true,
+			filter_hideFilters : true,
+			filter_ignoreCase : true,
+			filter_liveSearch : true,
+			filter_matchType : { 'input': 'exact', 'select': 'exact' },
+			filter_onlyAvail : 'filter-onlyAvail',
+			filter_placeholder : { search : 'Filter results...', select : '' },
+			filter_reset : 'button.reset',
+			filter_resetOnEsc : true,
+			filter_saveFilters : false,
+			filter_searchDelay : 420,
+			filter_searchFiltered: true,
+			filter_selectSource  : null,
+			filter_serversideFiltering : false,
+			filter_startsWith : false,
+			filter_useParsedData : false,
+			filter_defaultAttrib : 'data-value',
+			filter_selectSourceSeparator : '|',pager_output: '{startRow:input} to {endRow} of {totalRows} rows', // '{page}/{totalPages}'
+	        
+	        pager_updateArrows: true,
+	        pager_startPage: 0,
+	        pager_size: 10,
+	        pager_savePages: false,
+	        pager_fixedHeight: false,
+	        pager_removeRows: false, // removing rows in larger tables speeds up the sort
+	        pager_ajaxUrl: null,
+	        pager_customAjaxUrl: function(table, url) { return url; },
+	        pager_ajaxError: null,
+	        pager_ajaxObject: {
+	          dataType: 'json'
+	        },
+	        pager_ajaxProcessing: function(ajax){ return [ 0, [], null ]; },
+	        // css class names that are added
+	        pager_css: {
+	          container   : 'tablesorter-pager',    // class added to make included pager.css file work
+	          errorRow    : 'tablesorter-errorRow', // error information row (don't include period at beginning); styled in theme file
+	          disabled    : 'disabled'              // class added to arrows @ extremes (i.e. prev/first arrows "disabled" on first page)
+	        },
+	        // jQuery selectors
+	        pager_selectors: {
+	          container   : '.pager',       // target the pager markup (wrapper)
+	          first       : '.first',       // go to first page arrow
+	          prev        : '.prev',        // previous page arrow
+	          next        : '.next',        // next page arrow
+	          last        : '.last',        // go to last page arrow
+	          gotoPage    : '.gotoPage',    // go to page selector - select dropdown that sets the current page
+	          pageDisplay : '.pagedisplay', // location of where the "output" is displayed
+	          pageSize    : '.pagesize'     // page size selector - select dropdown that sets the "size" option
+	        }
+		}
+	}).bind('pagerChange pagerComplete pagerInitialized pageMoved', function(e, c){
+      var p = c.pager, // NEW with the widget... it returns config, instead of config.pager
+        msg = '"</span> event triggered, ' + (e.type === 'pagerChange' ? 'going to' : 'now on') +
+        ' page <span class="typ">' + (p.page + 1) + '/' + p.totalPages + '</span>';
+      $('#display')
+        .append('<li><span class="str">"' + e.type + msg + '</li>')
+        .find('li:first').remove();
+    });
+
+	var dataObject = {};
+	var rawDataArray = getDataFromURL(window.location.search.match(/data=([^&]*)/));
+
+    if (rawDataArray.length === 0) {
+        var cookieData = $.cookie('marketplaceData');
+        if (typeof cookieData !== 'undefined') {
+            dataObject = $.parseJSON(cookieData);
+        }
+        showTable(dataObject);
+    } else {
+    	$("#almostDone").show(500);
+        processRawData(rawDataArray);
+    }
+}
+
+function processRawData(rawDataArray) {
+    var dataObject = {};
+
+    var dataLen = rawDataArray.split("\n").length - 1;
+    var dataSplit = rawDataArray.split("\n");
+    for (var i=0; i<dataLen; i++) {
+    	var rowSplit = dataSplit[i].split(" ");
+    	var wordCount = parseInt(rowSplit[3]);
+    	var word = '';
+    	for (var j=1; j<wordCount; j++) {
+    		word += rowSplit[j+3] + " ";
+    	}
+    	word += rowSplit[wordCount+3];
+
+    	if (dataObject[word] == undefined) {
+    		dataObject[word] = {};
+    	}
+    	if (dataObject[word][rowSplit[0]] == undefined) {
+    		dataObject[word][rowSplit[0]] = {};
+    	}
+    	if (dataObject[word][rowSplit[0]][rowSplit[1] + " " + rowSplit[2]] == undefined) {
+    		dataObject[word][rowSplit[0]][rowSplit[1] + " " + rowSplit[2]] = [];
+    	}
+    	dataObject[word][rowSplit[0]][rowSplit[1] + " " + rowSplit[2]].push(rowSplit[wordCount+4]);
+    	dataObject[word][rowSplit[0]][rowSplit[1] + " " + rowSplit[2]].push(rowSplit[wordCount+5]);
+    	dataObject[word][rowSplit[0]][rowSplit[1] + " " + rowSplit[2]].push(rowSplit[wordCount+6]);
+    }
+
+	// console.log(dataObject);
+
+	//Create cookie of data
+	if (Object.size(dataObject) > 0) {
+		var stringify = JSON.stringify(dataObject);
+		$.cookie('marketplaceData', stringify, {
+	        expires: 30
+	    });
+	    setTimeout(function() {
+	    	window.location.replace("http://tsitu.github.io/MH-Tools/analyzer.html");
+	    }, 1000);
+	}
+}
+
+function showTable(dataObject) {
+	$("#almostDone").hide();
+	$("#pagerContainer").show(500);
+	$("#tableContainer").show(500);
+	var table = document.getElementById("table");
+	table.innerHTML = '';
+	var tableHTML = "<thead><tr><th>Date Closed</th><th>Item Name</th><th data-filter='false'>Action</th><th data-filter='false'>Quantity</th><th data-filter='false'>Unit Price</th><th data-filter='false'>Total</th></tr></thead><tbody>";
+	for (var itemName in dataObject) {
+		for (var action in dataObject[itemName]) {
+			for (var date in dataObject[itemName][action]) {
+				for (var i=0; i<dataObject[itemName][action][date].length/3; i++) {
+					tableHTML += "<tr><td>" + date + "</td><td>" + itemName + "</td><td>" + action + "</td><td>" + dataObject[itemName][action][date][i*3] + "</td><td>" + dataObject[itemName][action][date][i*3+1] + "</td><td>" + dataObject[itemName][action][date][i*3+2] + "</td></tr>";
+				}
+			}
+		}
+	}
+	tableHTML += "</tbody>";
+	table.innerHTML = tableHTML;
+
+	var resort = true, callback = function() {
+    	// empty
+    };
+	$("#table").trigger("updateAll", [ resort, callback ]);
+}
+
+/*
+ * Utilities
+ */
+function getDataFromURL(parameters) {
+    if (parameters) {
+        parameters = decodeURI(parameters[1]);
+
+        return parameters
+            .split('/')
+            .join('\n');
+    } else {
+        return [];
+    }
+}
+
+Object.size = function(obj) {
+    var size = 0;
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+}
---
+++ src/analyzerbookmarklet.js
@@ -0,0 +1,56 @@
+javascript:void(function() {
+	var url = "http://tsitu.github.io/MH-Tools/analyzer.html?data=";
+	var sellString = "div.history-details .dataTable .sell";
+	var buyString = "div.history-details .dataTable .buy";
+	var entries = document.querySelector("div.history-details .dataTables_info").innerHTML;
+    var entriesSlice = parseInt(entries.slice(entries.indexOf("entries")-3, entries.indexOf("entries")));
+    var entriesNum = Math.ceil(entriesSlice/10);
+    var counter = 0;
+    var newWindow = window.open('http://tsitu.github.io/MH-Tools/analyzerwaiting.html', '_blank');
+
+    var interval = setInterval(function() {
+        var a = document.querySelector("div.history-details .paginate_button.next");
+        if (a.className.indexOf("disabled") < 0 || parseInt(document.querySelector("div.history-details .paginate_button.current").innerHTML) == entriesNum) {
+        	var sell = document.querySelectorAll(sellString + " .sorting_1, " + sellString + " .item-name, " + sellString + " .numeric.quantity, " + sellString + " .numeric, " + sellString + " .numeric.total");
+		    var buy = document.querySelectorAll(buyString + " .sorting_1, " + buyString + " .item-name, " + buyString + " .numeric.quantity, " + buyString + " .numeric, " + buyString + " .numeric.total");
+
+		    var sellArr = [];
+		    var buyArr = [];
+
+		    for (var i=0; i<sell.length/5; i++) {
+		    	if (i == 0) {
+		    		sellArr.push("Sell " + sell[i].textContent + " " + (sell[i+1].textContent.split(" ").length) + " " + sell[i+1].textContent + " " + sell[i+2].textContent + " " + sell[i+3].textContent + " " + sell[i+4].textContent);
+		    	}
+		        else {
+		        	sellArr.push("Sell " + sell[i*5].textContent + " " + (sell[i*5+1].textContent.split(" ").length) + " " + sell[i*5+1].textContent + " " + sell[i*5+2].textContent + " " + sell[i*5+3].textContent + " " + sell[i*5+4].textContent);
+		    	}
+		    }
+
+		    for (var i=0; i<buy.length/5; i++) {
+		        if (i == 0) {
+		    		buyArr.push("Buy " + buy[i].textContent + " " + (buy[i+1].textContent.split(" ").length) + " " + buy[i+1].textContent + " " + buy[i+2].textContent + " " + buy[i+3].textContent + " " + buy[i+4].textContent);
+		    	}
+		        else {
+		        	buyArr.push("Buy " + buy[i*5].textContent + " " + (buy[i*5+1].textContent.split(" ").length) + " " + buy[i*5+1].textContent + " " + buy[i*5+2].textContent + " " + buy[i*5+3].textContent + " " + buy[i*5+4].textContent);
+		    	}
+		    }
+
+		    if (sellArr.length > 0) {
+		        url += encodeURI(sellArr.join('/'));
+		        url += "/";
+		    }
+
+		    if (buyArr.length > 0) {
+		        url += encodeURI(buyArr.join('/'));
+		        url += "/";
+		    }
+
+            a.click();
+        }
+        counter++;
+        if (counter === entriesNum || a.className.indexOf("disabled") >= 0 || a == null) {
+        	newWindow.location.assign(url);
+            clearInterval(interval);
+        }
+    }, 0);
+})();
