fa12ef58cbef60e9f80fea7ab02271ec21b4da19
Tran Situ <tsitu13@gmail.com>
2016-07-28 12:59:14 -0700

    Implemented map bookmarklet check for locations with periods

--- src/mapbookmarklet.js
+++ src/mapbookmarklet.js
@@ -11,6 +11,7 @@ javascript:void(function() {
 	var mice = [];
     var currLoc = document.getElementsByClassName("treasureMapPopup-mice-groups")[0].className;
     if (currLoc.indexOf("inotherenvironments") < 0) {
+        currLoc = currLoc.replace(/\./g, "\\.");
     	currLoc = currLoc.replace(" ", ".");
     	var uncaughtLoc = document.querySelectorAll("." + currLoc + " .treasureMapPopup-mice-group-mouse-name span");
     	for (var i=0; i<uncaughtLoc.length; i++) {
