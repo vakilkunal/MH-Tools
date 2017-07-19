54e2fd1cd2bd3e8e2aa48cf8c70c9548ff71b85d
Renette Ros <supergirl0894+git@gmail.com>
2017-07-19 22:42:53 +0200

    Update copied files

--- build/copyFiles.sh
+++ build/copyFiles.sh
@@ -2,6 +2,7 @@
 
 mkdir -p out/src/bookmarklet
 cp *.html out/
+cp -r data out/
 cp -r css out/
 cp -r font-awesome -r out/
 cp -r fonts out/
@@ -9,5 +10,6 @@ cp -r img out/
 cp -r lib out/
 cp -r src/main out/src/
 cp -r src/utils out/src/
+cp readme.md out/
 cp src/bookmarklet/*.min.js out/src/bookmarklet/
 
