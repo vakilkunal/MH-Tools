c946f88cf23def316084c584bd47b710c517c8e2
Renette Ros <supergirl0894+git@gmail.com>
2017-10-20 22:18:25 +0200

    BW Rift queries

--- sql/BWRiftQueries.sql
+++ sql/BWRiftQueries.sql
@@ -7,21 +7,21 @@
 -- Select Mice And Sample sizes
 -- Change Charm =/!= based on requirements
 SELECT
-  l.name   AS location,
-  s.name   AS phase,
-  c.name   AS cheese,
-  ch.name  AS charm,
-  m.name   AS mouse,
-  count(*) AS attractions
+  l.name       AS location,
+  s.name       AS phase,
+  c.name       AS cheese,
+#   ch.name      AS charm,
+  m.name       AS mouse,
+  count(*)     AS attractions
 FROM hunts h
   INNER JOIN locations l ON l.id = h.location_id
   INNER JOIN hunt_stage hs ON h.id = hs.hunt_id
   INNER JOIN stages s ON hs.stage_id = s.id
   INNER JOIN cheese c ON h.cheese_id = c.id
   INNER JOIN mice m ON h.mouse_id = m.id
-  INNER JOIN charms ch ON h.charm_id = ch.id
-WHERE location_id = 55 AND
-      h.charm_id = 2322 #--Rift Anti Skele
+#   INNER JOIN charms ch ON h.charm_id = ch.id
+WHERE h.location_id = 55 AND
+      h.charm_id != 2322 #--Rift Anti Skele
 GROUP BY h.location_id, hs.stage_id, c.id, m.id;
 
 -- Select Sample Sizes without mice
