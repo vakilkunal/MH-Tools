4159ebd346fdac7b273ba09c092103f010848348
Tran Situ <tsitu13@gmail.com>
2017-12-07 21:25:42 -0800

    Added Silver Crown Solver and Prettier 1.9.1

---
+++ src/bookmarklet/crownbookmarklet.js
@@ -0,0 +1,28 @@
+(function() {
+    if (location.href.indexOf("mousehuntgame.com") < 0) {
+        alert("You are not on mousehuntgame.com! Please try again.");
+        return;
+    }
+	if (document.querySelector("div.crownheader") == null) {
+        alert("Please navigate to \"King's Crowns'!\"");
+        return;
+	}
+
+    var miceNames = $(".bronze").parent().parent().find("img").map(function(){return $(this).attr("title")}).get();
+    var miceCatches = $(".bronze").map(function(){return $(this).text()}).get();
+
+    var combinedString = "";
+    for (var i = 0; i < miceNames.length; i++) {
+        combinedString += miceNames[i] + "\n" + miceCatches[i] + "\n";
+    }
+
+    var copyListener = function(event) {
+        document.removeEventListener("copy", copyListener, true);
+        event.preventDefault();
+        var clipboardData = event.clipboardData;
+        clipboardData.clearData();
+        clipboardData.setData("text/plain", combinedString);
+    };
+    document.addEventListener("copy", copyListener, true);
+    document.execCommand("copy");
+})();
