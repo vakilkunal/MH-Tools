bd99067e0a14395ae7429cf5c45ee00ace6130ab
Tran Situ <tsitu13@gmail.com>
2018-02-13 00:03:06 -0800

    Added Windows NT x64 Firefox 10.0 UA string to fix GitHub returning (presumably) mobile HTML

--- build/processTimestamp.js
+++ build/processTimestamp.js
@@ -24,11 +24,18 @@
 
   /**
    * Parses datetime data from master/src/bookmarklet HTML
-   * @param {string} url
+   * @param {string} htmlUrl
    */
-  function fetchTimestamps(url) {
+  function fetchTimestamps(htmlUrl) {
     return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
+      const options = {
+        url: htmlUrl,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"
+        }
+      };
+      request(options, (error, response, body) => {
         if (error) {
           reject(Error(error));
         } else if (response.statusCode !== 200) {
