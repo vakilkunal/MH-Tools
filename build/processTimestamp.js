54a22ceb796dbbbe62187f9fb35aa87d375aeccb
Tran Situ <tsitu13@gmail.com>
2018-02-19 22:27:40 -0800

    Rewrote processTimestamp to use Puppeteer

--- build/processTimestamp.js
+++ build/processTimestamp.js
@@ -1,6 +1,6 @@
 (function() {
-  const request = require("request");
   const cheerio = require("cheerio");
+  const puppeteer = require("puppeteer");
 
   const fileUtils = require("./modules/fileUtils");
   const bookmarkletList = [
@@ -12,42 +12,42 @@
     "setup"
   ];
 
-  fetchTimestamps(
-    "https://github.com/tsitu/MH-Tools/tree/master/src/bookmarklet"
-  ).then(res => {
-    const bookmarkletJson = {};
-    for (let i = 0; i < bookmarkletList.length; i++) {
-      bookmarkletJson[bookmarkletList[i]] = res[i];
-    }
-    fileUtils.saveJsonFile("data/bookmarklet-timestamps.json", bookmarkletJson);
-  });
-
   /**
    * Parses datetime data from master/src/bookmarklet HTML
    * @param {string} htmlUrl
    */
-  function fetchTimestamps(htmlUrl) {
-    return new Promise((resolve, reject) => {
-      const options = {
-        url: htmlUrl,
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"
-        }
-      };
-      request(options, (error, response, body) => {
-        if (error) {
-          reject(Error(error));
-        } else if (response.statusCode !== 200) {
-          reject(Error(response.statusCode));
-        } else {
-          const $ = cheerio.load(body);
-          const result = $(".css-truncate-target [datetime]")
-            .map((i, el) => $(el).text())
-            .get();
-          resolve(result);
-        }
-      });
+  async function fetchTimestamps(htmlUrl) {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: "google-chrome-beta"
+      // executablePath:
+      //   "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
+
+    const page = await browser.newPage();
+    await page.goto(htmlUrl);
+
+    const body = await page.content();
+    await browser.close();
+
+    const $ = cheerio.load(body);
+    const result = $("time-ago")
+      .map((i, el) => $(el).attr("title"))
+      .get();
+    return result;
   }
+
+  fetchTimestamps(
+    "https://github.com/tsitu/MH-Tools/tree/master/src/bookmarklet"
+  ).then(res => {
+    const format = res.map(el => {
+      const spl = el.split(", ");
+      return `${spl[0]}, ${spl[1]}`;
+    });
+    const bookmarkletJson = {};
+    for (let i = 0; i < bookmarkletList.length; i++) {
+      bookmarkletJson[bookmarkletList[i]] = format[i];
+    }
+    fileUtils.saveJsonFile("data/bookmarklet-timestamps.json", bookmarkletJson);
+  });
 })();
