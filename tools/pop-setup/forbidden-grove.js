ad3f5236f569e4fe953e57780db52ee9eaa159e2
Geno Roupsky <geno@roupsky.name>
2018-02-24 20:27:07 +0200

    Tool for extracting population data for Forbidden Grove from Jack's db

---
+++ tools/pop-setup/forbidden-grove.js
@@ -0,0 +1,21 @@
+const utils = require("../_utils")
+
+const cheeses = [
+  'Ancient',
+  'Moon',
+  'Crescent',
+  'Radioactive Blue',
+  'Rancid Radioactive Blue'
+]
+
+module.exports = {
+  default: {
+    location: utils.genVarField('location', 'Forbidden Grove'),
+    cheese: utils.genVarField('cheese', cheeses)
+  },
+  series: [
+    { // stages
+      phase: utils.genVarField('stage', [ 'open', 'closed' ])
+    }
+  ]
+}
