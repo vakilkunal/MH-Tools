1d9259fc94268f4501ff4f61699d7b5b39f8b908
Tran Situ <trancsitu@gmail.com>
2018-10-14 17:02:52 -0700

    Updated Map bookmarklet to use AJAX instead of Selectors
    Eliminates need to have map open

--- src/bookmarklet/mapbookmarklet.js
+++ src/bookmarklet/mapbookmarklet.js
@@ -1,26 +1,60 @@
 (function() {
-  if (location.href.indexOf("mousehuntgame.com") < 0) {
-    alert("You are not on mousehuntgame.com! Please try again.");
-    return;
-  }
-  if (!document.querySelector("div.treasureMapPopupContainer.viewMap")) {
-    alert("Please navigate to 'Active Map'!");
+  if (!window.jQuery) {
+    alert("jQuery does not seem to be enabled on this page");
     return;
   }
 
-  var mice = $(".treasureMapPopup-goals-group-goal:not(.complete)")
-    .map(function() {
-      var data = $(this).context.textContent;
-      // Thunderlord lightning emoji edge case
-      if (data.indexOf("Thunderlord") >= 0) {
-        return "Thunderlord";
+  var payload = {
+    map_id: user.quests.QuestRelicHunter.default_map_id,
+    action: "map_info",
+    uh: user.unique_hash,
+    last_read_journal_entry_id: lastReadJournalEntryId
+  };
+
+  // Extract map mice from a map
+  function getMapMice(data, uncaught_only) {
+    var mice = [];
+    $.each(data.treasure_map.groups, function(key, group) {
+      if (uncaught_only && group.name.indexOf("Uncaught mice ") === -1) {
+        return;
       }
-      return data;
-    })
-    .toArray();
-  var url = "https://tsitu.github.io/MH-Tools/map.html";
-  url += "?mice=" + encodeURIComponent(mice.join("/"));
 
-  var newWindow = window.open("", "mhmapsolver");
-  newWindow.location = url;
+      $.each(group.goals, function(key, mouse) {
+        // Thunderlord lightning emoji edge case
+        if (mouse.name.indexOf("Thunderlord") !== -1) {
+          mice.push("Thunderlord");
+        } else {
+          mice.push(mouse.name);
+        }
+      });
+    });
+    return mice;
+  }
+
+  $.post(
+    "https://www.mousehuntgame.com/managers/ajax/users/relichunter.php",
+    payload,
+    null,
+    "json"
+  ).done(function(data) {
+    if (data) {
+      if (!data.treasure_map || data.treasure_map.view_state === "noMap") {
+        alert(
+          "Please make sure you are logged into MH and are currently on a treasure map"
+        );
+        return;
+      }
+
+      if (data.treasure_map.map_class != "treasure") {
+        alert("This map class is not yet supported");
+        return;
+      }
+
+      var mice = getMapMice(data, true);
+      var url = "https://tsitu.github.io/MH-Tools/map.html";
+      url += "?mice=" + encodeURIComponent(mice.join("/"));
+      var newWindow = window.open("", "mhmapsolver");
+      newWindow.location = url;
+    }
+  });
 })();
