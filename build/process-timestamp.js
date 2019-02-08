1662964409011d3ba4f35c5b1a3d1487cea5028c
Tran Situ <trancsitu@gmail.com>
2019-02-07 17:27:06 -0800

    Added an initial throwaway page load to process-timestamp.js

--- build/process-timestamp.js
+++ build/process-timestamp.js
@@ -29,16 +29,23 @@
       //   "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     });
 
+    // Initial throwaway page: "Failed to load latest commit information"
+    const initPage = await browser.newPage();
+    await initPage.goto(htmlUrl);
+    const initBody = await initPage.content();
+    await initPage.close();
+
     const page = await browser.newPage();
     await page.goto(htmlUrl);
-
     const body = await page.content();
+    await page.close();
     await browser.close();
 
     const $ = cheerio.load(body);
     const result = $("time-ago")
       .map((i, el) => $(el).attr("title"))
       .get();
+
     return result;
   }
 
@@ -49,10 +56,13 @@
       const spl = el.split(", ");
       return `${spl[0]}, ${spl[1]}`;
     });
+
     const bookmarkletJson = {};
     for (let i = 0; i < bookmarkletList.length; i++) {
       bookmarkletJson[bookmarkletList[i]] = format[i];
     }
+    console.log(bookmarkletJson);
+
     fileUtils.saveJsonFile(
       "data/json/bookmarklet-timestamps.json",
       bookmarkletJson
