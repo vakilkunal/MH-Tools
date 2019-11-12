761cda979adb4009610c05a444fe09505d82aa23
Tran Situ <trancsitu@gmail.com>
2019-11-11 22:05:32 -0800

    Quick bm-cre URL fix

--- src/bookmarklet/bm-cre.js
+++ src/bookmarklet/bm-cre.js
@@ -560,8 +560,8 @@
   }
 
   function sendData(parameters) {
-    // var url = "https://tsitu.github.io/MH-Tools/cre.html?";
-    var url = "http://localhost:8000/cre.html?";
+    var url = "https://tsitu.github.io/MH-Tools/cre.html?";
+    // var url = "http://localhost:8000/cre.html?";
 
     for (var key in parameters) {
       var value = encodeURIComponent(parameters[key]);
